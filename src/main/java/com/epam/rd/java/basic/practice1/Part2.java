4c934d9d09ad9f14f03caa7dc6c65d7eeb94bf5c
iraCoMFF <iracomff@email.com>
2021-03-16 15:38:17 +0000

    Update Part2.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part2.java
@@ -6,7 +6,7 @@ public class Part2 {
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
         int sum = x +  y;
-        System.out.println(sum);
+        System.out.print(sum);
     }
 	
 }
