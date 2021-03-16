4df497d950514ddf906579c2fe377afebe88cd36
iraCoMFF <iracomff@email.com>
2021-03-16 21:07:49 +0000

    Update Part3.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part3.java
@@ -3,7 +3,7 @@ package com.epam.rd.java.basic.practice1;
 public class Part3 {
 
     public static void main(String[] args) {
-         for (int i = 0; i < args.length; i++) {
+         for (int i = 0; i < args.length-1; i++) {
           System.out.print(args[i]);
           System.out.print(" ");
       }
