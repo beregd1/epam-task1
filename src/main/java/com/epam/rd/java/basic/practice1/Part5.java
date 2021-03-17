464b63441ba6dde249ebfc780a027a269bb5d949
iraCoMFF <iracomff@email.com>
2021-03-17 03:22:39 +0000

    Update Part5.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part5.java
@@ -3,7 +3,13 @@ package com.epam.rd.java.basic.practice1;
 public class Part5 {
 
     public static void main(String[] args) {
-        
+        int x = Integer.parseInt(args[0]);
+          int sum = 0;
+          while (x != 0) {
+          sum += x % 10;
+          x /= 10;
+        }
+      System.out.print(sum);
     }
 	
 }
