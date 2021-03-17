7f37782d4e3302abd2733509f71f0b2a4a121398
iraCoMFF <iracomff@email.com>
2021-03-17 00:29:39 +0000

    Update Part4.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -3,7 +3,15 @@ package com.epam.rd.java.basic.practice1;
 public class Part4 {
 
     public static void main(String[] args) {
-        
+    int x = Integer.parseInt(args[0]);
+    int y = Integer.parseInt(args[1]);
+       System.out.print("NOD " + x + " and " + y + " = ");
+      while (y != 0) {
+      int tmp = x % y;
+       x = y;
+        y = tmp;
     }
+     System.out.println(x);    
+   }
 
 }
