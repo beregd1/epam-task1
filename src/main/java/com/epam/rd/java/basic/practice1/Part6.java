f35074410d34912ab60226b745d8219adb148ab5
iraCoMFF <iracomff@email.com>
2021-03-17 05:27:53 +0000

    Update Part6.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part6.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part6.java
@@ -3,7 +3,8 @@ package com.epam.rd.java.basic.practice1;
 public class Part6 {
 
     public static void main(String[] args) {
-        
+       int n = Intrger.parseInt(args[0]);
+       int[]arr = new int[n];
     }
 	
 }
